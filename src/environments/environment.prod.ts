6c7679010f56d2f778a180a5a74da843bc35d39c
Nabildexter <36948041+Nabildexter@users.noreply.github.com>
2022-09-30 16:12:53 -0700

    reset repository

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
-  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
-  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue: '8838a9bedbmshb14c70d3c0f8461p181c77jsn5264c1fbe2f3'
-};
